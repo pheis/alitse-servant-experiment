94bd43987a5da11cba1a416e79eacd058043c59c
Pyry Heiskanen <pyry.heiskanen@gmail.com>
2019-07-29 23:29:51 +0300

    Format

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,11 +1,11 @@
 module Lib where
 
-import qualified Crypto.Random as Random
+import qualified Crypto.Random                 as Random
 
 import qualified Data.Aeson                    as Aeson
 import qualified Data.ByteString               as B
 import qualified Data.ByteString.Lazy          as BL
-import qualified Data.ByteString.Base64.URL as Base64URL
+import qualified Data.ByteString.Base64.URL    as Base64URL
 import qualified Data.Either                   as Either
 import qualified Data.Maybe                    as Maybe
 import qualified Data.Text                     as T
@@ -16,17 +16,20 @@ import qualified Database.Redis                as Redis
 newtype Resources a = Resources { resources :: a }
 
 generateId :: IO T.Text
-generateId = let len = 32 in do
-    bytes <- Random.getRandomBytes len :: IO B.ByteString
-    return $ TE.decodeUtf8 $ B.take len $ Base64URL.encode bytes
+generateId =
+    let len = 32
+    in  do
+            bytes <- Random.getRandomBytes len :: IO B.ByteString
+            return $ TE.decodeUtf8 $ B.take len $ Base64URL.encode bytes
 
 toResources :: Aeson.FromJSON a => Either Redis.Reply [B.ByteString] -> [a]
 toResources (Right xs) = Maybe.mapMaybe Aeson.decodeStrict xs
 toResources _          = error "something went wrong"
 
-toResource :: Aeson.FromJSON a => Either Redis.Reply (Maybe B.ByteString) -> Maybe a
+toResource
+    :: Aeson.FromJSON a => Either Redis.Reply (Maybe B.ByteString) -> Maybe a
 toResource (Right (Just x)) = Aeson.decodeStrict x
-toResource _              = error "something went wrong"
+toResource _                = error "something went wrong"
 
 -- fromJson x = fmap (fmap Aeson.decodeStrict) x
 
@@ -35,20 +38,32 @@ toJson :: Aeson.ToJSON a => a -> B.ByteString
 toJson = BL.toStrict . Aeson.encode
 
 
-hset :: Aeson.ToJSON a => Redis.Connection -> B.ByteString -> T.Text -> a -> IO a
+hset
+    :: Aeson.ToJSON a => Redis.Connection -> B.ByteString -> T.Text -> a -> IO a
 hset conn storeName key value = do
-    _ <- Redis.runRedis conn $ Redis.hset storeName (TE.encodeUtf8 key) $ toJson value
+    _ <- Redis.runRedis conn $ Redis.hset storeName (TE.encodeUtf8 key) $ toJson
+        value
     return value
 
-create :: Aeson.ToJSON b => T.Text -> (a -> T.Text -> b) -> Redis.Connection -> a -> IO b
+create
+    :: Aeson.ToJSON b
+    => T.Text
+    -> (a -> T.Text -> b)
+    -> Redis.Connection
+    -> a
+    -> IO b
 create storeName toStorableValue conn newValue = do
     newId <- generateId
     hset conn (TE.encodeUtf8 storeName) newId $ toStorableValue newValue newId
 
 readAll :: Aeson.FromJSON a => T.Text -> Redis.Connection -> IO [a]
-readAll storeName conn = Redis.runRedis conn $ toResources <$> Redis.hvals (TE.encodeUtf8 storeName)
+readAll storeName conn =
+    Redis.runRedis conn $ toResources <$> Redis.hvals (TE.encodeUtf8 storeName)
 
-readOne :: Aeson.FromJSON a => T.Text -> Redis.Connection -> T.Text -> IO (Maybe a)
-readOne storeNameText conn keyText = Redis.runRedis conn $ toResource <$> Redis.hget storeName key
-    where key = TE.encodeUtf8 keyText
-          storeName = TE.encodeUtf8 storeNameText
+readOne
+    :: Aeson.FromJSON a => T.Text -> Redis.Connection -> T.Text -> IO (Maybe a)
+readOne storeName conn key =
+    Redis.runRedis conn $ toResource <$> Redis.hget storeName' key'
+  where
+    key'       = TE.encodeUtf8 key
+    storeName' = TE.encodeUtf8 storeName
